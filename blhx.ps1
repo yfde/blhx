83351322f78bdeeead4be010b9f8d07fa102f90e
Yufan Deng <yf-deng@outlook.com>
2023-10-04 19:47:34 +0800

    update ps1

--- blhx.ps1
+++ blhx.ps1
@@ -1,2 +1,2 @@
-Start-Process -filepath "F:\Program Files (x86)\Nemu\EmulatorShell\nemulauncher.exe"
-Start-Process powershell -verb RunAs -ArgumentList "cd 'D:\OneDrive - HKUST Connect\Atom_Workspace\blhx' ; python blhx_auto.py ; pause"
+Start-Process -filepath "F:\Program Files\Netease\MuMuPlayer-12.0\shell\MuMuPlayer.exe"
+Start-Process powershell -verb RunAs -ArgumentList "cd 'F:\blhx' ; python blhx_auto.py ; pause"
